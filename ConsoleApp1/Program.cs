ba359b953fe340cb65d7918cd721bd4b8d4f20f9
Sam Magura <srmagura@gmail.com>
2022-01-21 14:56:53 -0500

    various

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,53 +1,49 @@
 ﻿using Aspose.Pdf;
-using Aspose.Pdf.Annotations;
 using System;
 using System.IO;
 using System.Reflection;
 
-namespace ConsoleApp1
+namespace ConsoleApp1;
+
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            var pdfLicense = new License();
+        var pdfLicense = new License();
 
-            var assembly = Assembly.GetExecutingAssembly();
-            var licenseFileName = $"{assembly.GetName().Name}.License.Aspose.Total.NET.lic";
+        var assembly = Assembly.GetExecutingAssembly();
+        var licenseFileName = $"{assembly.GetName().Name}.Aspose.Total.NET.lic";
 
-            using var stream = assembly.GetManifestResourceStream(licenseFileName);
-            pdfLicense.SetLicense(stream);
+        using var stream = assembly.GetManifestResourceStream(licenseFileName);
+        pdfLicense.SetLicense(stream);
 
-            var outputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ReportBuilderTests");
-            Directory.CreateDirectory(outputDirectory);
+        var outputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProgramOutput");
+        Directory.CreateDirectory(outputDirectory);
 
-            var path = Path.Combine(outputDirectory, "DrawingSet.pdf");
+        var path = Path.Combine(outputDirectory, "Output.pdf");
 
-            using (var outputStream = File.OpenWrite(path))
-            {
-                Document doc = new Document();
+        using var outputStream = File.OpenWrite(path);
+        using var doc = new Document();
 
-                var titlePage = doc.Pages.Add();
-                titlePage.PageInfo.Margin = new MarginInfo(0, 0, 0, 0);
-                titlePage.SetPageSize(17 * 72, 11 * 72);
+        var titlePage = doc.Pages.Add();
+        titlePage.PageInfo.Margin = new MarginInfo(0, 0, 0, 0);
+        titlePage.SetPageSize(17 * 72, 11 * 72);
 
-                // Create Graph instance
-                var graph = new Aspose.Pdf.Drawing.Graph((float)titlePage.PageInfo.Width, (float)titlePage.PageInfo.Height);
+        // Create Graph instance
+        var graph = new Aspose.Pdf.Drawing.Graph((float)titlePage.PageInfo.Width, (float)titlePage.PageInfo.Height);
 
-                // Add graph object to paragraphs collection of page instance
-                titlePage.Paragraphs.Add(graph);
+        // Add graph object to paragraphs collection of page instance
+        titlePage.Paragraphs.Add(graph);
 
-                // Create Rectangle instance
-                var rect = new Aspose.Pdf.Drawing.Rectangle(10, 10, 100, 100);
+        // Create Rectangle instance
+        var rect = new Aspose.Pdf.Drawing.Rectangle(10, 10, 100, 100);
 
-                // Specify fill color for Graph object
-                rect.GraphInfo.FillColor = Color.Blue;
+        // Specify fill color for Graph object
+        rect.GraphInfo.FillColor = Color.Blue;
 
-                // Add rectangle object to shapes collection of Graph object
-                graph.Shapes.Add(rect);
+        // Add rectangle object to shapes collection of Graph object
+        graph.Shapes.Add(rect);
 
-                doc.Save(outputStream);
-            }
-        }
+        doc.Save(outputStream, SaveFormat.Pdf);
     }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+This repository demonstrates an issue with Aspose.PDF where Aspose fails to draw a rectangle.
+
+## Reproduction Steps
+
+1. Place an Aspose Total .NET license file at `ConsoleApp1/Aspose.Total.NET.lic`.
+2. Open the solution in Visual Studio 2022.
+3. Open the properties for the license file and set the build action to "Embedded Resource".
+
+WORK IN PROGRESS
